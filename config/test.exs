03e3a61b13b46c5812acff9adcbcdfc042d72e4b
chirchir <chirchir7370@gmail.com>
2024-08-06 18:14:31 +0300

    disable logs in tests env

--- config/test.exs
+++ config/test.exs
@@ -27,7 +27,7 @@ config :fundsjet, Fundsjet.Mailer, adapter: Swoosh.Adapters.Test
 config :swoosh, :api_client, false
 
 # Print only warnings and errors during test
-config :logger, level: :warning
+config :logger, level: :none
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
